a10896f0396ae284ab2b0dab416d789ce4b81b8a
Khong Ngoc Anh <khongngocanh2004@gmail.com>
2024-10-10 22:38:37 +0700

    add graph

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 import pickle
-import copy
+from tqdm import tqdm
 
 from pathlib import Path
 
@@ -35,8 +39,8 @@ def state_space_initalize(size):
     return all_states, rewards
 
 
-def policy_evaluation(all_states, rewards, values, policy):
-    
+def policy_evaluation(all_states, rewards, values, policy, GAMMA):
+    EPSILON = 1e-6 
     loop = 0
     while True:
         loop += 1
@@ -50,35 +54,17 @@ def policy_evaluation(all_states, rewards, values, policy):
                         rewards[next_state][1] + GAMMA * values[next_state][1]]
             values[state] = [new_value[0], new_value[1]]
             DELTA = max(DELTA, abs(temp[0] - values[state][0]), abs(temp[1] - values[state][1]))
-        print(f'Evaluation loop {loop}, delta={DELTA}')
+        # print(f'Evaluation loop {loop}, delta={DELTA}')
         if DELTA < EPSILON:
             break
-    print("Evaluation Done")
+    # print("Evaluation Done")
     
 
-
-if __name__ == '__main__':
-    INIT_SIZE = 4
-    all_states, rewards  = state_space_initalize(INIT_SIZE)
-
-    values = dict.fromkeys(all_states.keys(), [0.1, 0.1])
-    for state in all_states:
-        if all_states[state].is_end():
-            values[state] = [0, 0]
-
-    policy = dict.fromkeys(all_states.keys(), 0)
-
-    # Random Policy
-    for state in all_states:
-        if not all_states[state].is_end():
-            policy[state] = np.random.choice(all_states[state].get_next_states())
-
-    EPSILON = 0.1
-    GAMMA = 0.1
+def policy_improvement(all_states, rewards, values, policy, GAMMA):
     # Policy Improvement
     while True:
         # Policy Evaluation
-        policy_evaluation(all_states, rewards, values, policy)
+        policy_evaluation(all_states, rewards, values, policy, GAMMA)
         policy_stable = True
         for state in all_states:
             player = all_states[state].player
@@ -95,8 +81,81 @@ if __name__ == '__main__':
         if policy_stable:
             break
 
-    print("Policy Improvement Done")
-    print(policy)
+    # print("Policy Improvement Done")
+    # print(policy)
                     
     with open(f'policy_{INIT_SIZE}.pkl', "wb") as f:
         pickle.dump(policy, f)
+
+
+def random_eval(all_states, policy):
+    win = 0
+    loss = 0
+    draw = 0
+
+    for _ in range(10000):
+        player = np.random.choice([1, -1])
+        opponent = -player
+        current_state = State(INIT_SIZE)
+        
+        while not current_state.is_end():
+            if current_state.player == player:
+                current_state = all_states[policy[current_state.__hash__()]]
+            else:
+                current_state = all_states[np.random.choice(current_state.get_next_states())]
+
+        if current_state.winner == player:
+            win += 1    
+        elif current_state.winner == opponent:
+            loss += 1
+        else:
+            draw += 1
+
+    # print(f"Win: {win}, Loss: {loss}, Draw: {draw}")
+    print(f"Win rate: {win/10000}")
+    return win/10000, loss/10000 
+
+if __name__ == '__main__':
+    INIT_SIZE = 3
+    all_states, rewards  = state_space_initalize(INIT_SIZE)
+
+    values = dict.fromkeys(all_states.keys(), [0.1, 0.1])
+    for state in all_states:
+        if all_states[state].is_end():
+            values[state] = [0, 0]
+
+    policy = dict.fromkeys(all_states.keys(), 0)
+
+    # # Random Policy
+    # for state in all_states:
+    #     if not all_states[state].is_end():
+    #         policy[state] = np.random.choice(all_states[state].get_next_states())
+
+    wr_list = []
+    lr_list = []
+
+    for i in tqdm(range(10)):
+        # Random Policy
+        for state in all_states:
+            if not all_states[state].is_end():
+                policy[state] = np.random.choice(all_states[state].get_next_states())
+        policy_improvement(all_states, rewards, values, policy, GAMMA=0.1 * (i + 1))
+        win_rate, lose_rate = random_eval(all_states, policy)
+        wr_list.append(win_rate)
+        lr_list.append(lose_rate)
+
+    print(wr_list)
+    print(lr_list)
+
+    df = pd.DataFrame({
+        "Win Rate": wr_list,
+        "Lose Rate": lr_list,
+    })
+    df.set_index(pd.Index([0.1 * (i + 1) for i in range(10)]), inplace=True)
+
+    plt.ylim(0, 1)
+    plot = sns.lineplot(data=df)
+
+    save_plot = plot.figure
+    
+    save_plot.savefig("win_rate.png")
